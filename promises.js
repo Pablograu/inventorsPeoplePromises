fb1d7aeef029701e438ce5c7105006a183864e36
Pablograu <paugf89@gmail.com>
2019-10-02 19:30:00 +0200

    display repos done

--- promises.js
+++ promises.js
@@ -1,42 +1,60 @@
 /* Initial HTML Tags */
-const inputUser = document.querySelector('#search');
-const searchButton = document.querySelector('#button');
-const container = document.querySelector('.container');
+const inputValue = document.querySelector("#search");
+const searchButton = document.querySelector("#button");
+const container = document.querySelector(".container");
+const reposContainer = document.querySelector(".profile-repos");
+let reposArray = [];
 
-/* API Base URL */
-const baseUrl = 'https://api.github.com/users/';
-
-/* Get repos from Github API */
-const getUserRepos = async (username) => {
-  const api_call = await fetch(`${baseUrl + username}/repos`);
-  const data = await api_call.json();
-  return { data }
+const handleUserNameError = (str) => {
+  let errorDiv = document.createElement("div");
+  errorDiv.innerHTML = (`oopsi, the user ${str} does not exist`)
+  reposContainer.appendChild(errorDiv)
 };
 
-/* Event listener on click */
-searchButton.addEventListener("click", async () => {
-  // cleanErrorMessage();
-  // cleanUserDetails();
-  // cleanRepos();
-  const data = await getUserData(inputUser.value);
-  if (data.userDetails.message) {
-    const errorContainer = handleCreateTag("div", container);
-    errorContainer.className = "error-message"
-    handleCreateTag("P", errorContainer).innerHTML = data.userDetails.message;
-  } else {
-    renderUserDetails(data);
-    renderReposList(data);
-  };
-});
 
-/* Handles creation of html tags to show user's repos */
-const renderReposList = ({ reposList }) => {
-  //"Repositories Title Section"
-  const reposTitleSection = handleCreateTag("h3", container);
-  reposTitleSection.innerHTML = "Repositories";
-  reposTitleSection.className = "repo-title-section";
+const handleReposError = (str) => {
+  let errorDiv = document.createElement("div");
+  errorDiv.innerHTML = (`oopsi, the user ${str} does not have any repo at the moment`)
+  reposContainer.appendChild(errorDiv)
+}
 
-  return reposList.data.forEach(repo => {
-    createRepoCard(repo)
+const clearData = () => {
+  console.log('data cleared')
+  reposContainer.innerHTML = ''
+}
+
+const displayReposList = () => {
+  clearData()
+  reposArray.forEach(el => {
+    let reposDiv = document.createElement("p");
+    reposDiv.innerHTML = el;
+    reposContainer.appendChild(reposDiv);
+  });
+  reposArray = []
+};
+
+const fetchUsers = async user => {
+  const api_call = await fetch(`https://api.github.com/users/${user}/repos`);
+  const data = await api_call.json();
+
+  return { data };
+};
+
+const showData = () => {
+  fetchUsers(inputValue.value)
+  .then(response => {
+    console.log('STATUS:', response.body, response.data);
+    if (response.data.length > 0) {
+      response.data.map(e => {
+        reposArray.push(e.name);
+      });
+      displayReposList();
+    } else {
+      handleUserNameError(inputValue.value)
+    }
   });
 };
+
+searchButton.addEventListener("click", () => {
+  showData();
+});
